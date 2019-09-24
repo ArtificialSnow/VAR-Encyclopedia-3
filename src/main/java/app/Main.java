6f9be7e4b4f8ae34b4d897f6cc5170e5e3bdab68
Peter Lindsay <plin213@en-368401.uoa.auckland.ac.nz>
2019-09-24 17:56:48 +1200

    AudioChunksScene

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,18 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
-    private Stage _primaryStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        _primaryStage = primaryStage;
 
         Parent root = FXMLLoader.load(getClass().getResource("/main/resources/scenes/MainMenuScene.fxml"));
         primaryStage.setScene(new Scene(root, 1000, 500));
-        //primaryStage.setTitle("VAR Encyclopedia");
+        primaryStage.setResizable(false);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
     }
 
