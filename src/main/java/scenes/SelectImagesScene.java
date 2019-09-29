bfed09011109725576e710ef563e734c699d1e27
lhsunyc <lhsunyc@outlook.com>
2019-09-29 18:42:24 +1300

    add methods in SelectImageScene class

--- src/main/java/scenes/SelectImagesScene.java
+++ src/main/java/scenes/SelectImagesScene.java
@@ -2,14 +2,16 @@ package main.java.scenes;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import main.java.app.CreationFactory;
 import main.java.app.SceneType;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SelectImagesScene extends ApplicationScene {
 
@@ -21,14 +23,55 @@ public class SelectImagesScene extends ApplicationScene {
     @FXML private Button _shiftImageDownButton;
     @FXML private Button _createCreationButton;
     @FXML private TextField _creationName;
+    @FXML private ListView<String> _imageDownloand;
+    @FXML private ListView<String> _imageSelected;
 
     private String _searchTerm;
     private CreationFactory _creationFactory;
 
+
     //"./VAR-Encyclopedia/.temp/tempCombinedChunks.wav"
+    //"./VAR-Encyclopedia/downloads"
     @FXML
     public void initialize() {
         _creationFactory = new CreationFactory();
+
+        File[] downloadList = new File("./downloads").listFiles();
+        ArrayList<String> nameOfImages = new ArrayList<String>();
+        for (File searchTermDirectory : downloadList) {
+           nameOfImages.add(searchTermDirectory.getName());
+            _imageDownloand.getItems().add(searchTermDirectory.getName());
+        }
+
+        Image[] imageList = new Image[downloadList.length];
+        for (int i = 0; i < downloadList.length;i++ ) {
+            File image = new File("./downloads/"+downloadList[i].getName());
+            String filelocation = image.toURI().toString();
+            Image fxImage = new Image(filelocation);
+            imageList[i]=fxImage;
+        }
+
+        _imageDownloand.setCellFactory(param -> new ListCell<String>(){
+            private ImageView imageview = new ImageView();
+            @Override
+            public void updateItem(String name, boolean empty){
+                super.updateItem(name,empty);
+                if(empty){
+                    setText(null);
+                    setGraphic(null);
+                }
+                else {
+                    for (int i = 0; i < downloadList.length;i++ ) {
+                        if (name.equals(nameOfImages.get(i))){
+                        imageview.setImage(imageList[i]);
+
+                    }
+                        setText(name);
+                        setGraphic(imageview);
+                    }
+                }
+            }
+        });
     }
 
     public void setSearchTerm(String searchTerm) {
@@ -36,25 +79,58 @@ public class SelectImagesScene extends ApplicationScene {
     }
 
     public void addImageButtonHandler() {
+        String imageToAdd = _imageDownloand.getSelectionModel().getSelectedItem();
 
+        if (imageToAdd == null) {
+            createInformationAlert("No image Selected", "Please select a image");
+        } else {
+            if (_imageSelected.getItems().contains(imageToAdd)) {
+                createInformationAlert("image already added", "Image " + imageToAdd + " already added");
+            } else {
+                _imageSelected.getItems().add(imageToAdd);
+            }
+        }
     }
 
-    public void removeImageButtonHandler() {
-
+    public void removeImageButtonHandler() { String imageToAdd = _imageDownloand.getSelectionModel().getSelectedItem();
+        String imageToRemove = _imageDownloand.getSelectionModel().getSelectedItem();
+        if (imageToRemove == null) {
+            createInformationAlert("No Audio Chunk Selected", "Please select an Audio Chunk");
+        } else {
+            _imageSelected.getItems().remove(imageToRemove);
+        }
     }
 
     public void shiftImageUpButtonHandler() {
-
+        int selectedImage = _imageSelected.getSelectionModel().getSelectedIndex();
+        if (selectedImage == -1) {
+            createInformationAlert("No Image selected", "Please select a image");
+        } else if (selectedImage == 0){
+            createInformationAlert("Cannot shift image up further", "Cannot shift image up further");
+        } else {
+            String audioChunk = _imageSelected.getItems().get(selectedImage);
+            _imageSelected.getItems().remove(selectedImage);
+            _imageSelected.getItems().add((selectedImage - 1), audioChunk);
+        }
     }
 
     public void shiftImageDownButtonHandler() {
-
+        int selectedImage = _imageSelected.getSelectionModel().getSelectedIndex();
+        if (selectedImage == -1) {
+            createInformationAlert("No Image selected", "Please select a image");
+        } else if (selectedImage == _imageSelected.getItems().size() - 1){
+            createInformationAlert("Cannot shift image down further", "Cannot shift image down further");
+        } else {
+            String audioChunk = _imageSelected.getItems().get(selectedImage);
+            _imageSelected.getItems().remove(selectedImage);
+            _imageSelected.getItems().add((selectedImage + 1), audioChunk);
+        }
     }
 
     public void createCreationButtonHandler() {
-       // _creationFactory.combineImagesToVideo();
-       // _creationFactory.combineVideoAndText();
-       // _creationFactory.combineVideoAndAudio();
+        _creationFactory.combineImagesToVideo(_imageSelected.getItems().size());
+        _creationFactory.combineVideoAndText(_searchTerm);
+        _creationFactory.combineVideoAndAudio(_creationName.getText());
     }
 
     public void homeButtonHandler(ActionEvent event) throws IOException {
--- src/main/resources/shellscripts/combineImageFile.sh
+++ src/main/resources/shellscripts/combineImageFile.sh
@@ -2,4 +2,4 @@
 # operand 1 is duration of vedio
 # operand 2 is number of pictures
 
-ffmpeg -framerate $((${1}/${2})) -pattern_type glob -i 'VAR-Encyclopedia/downloads/*.jpg' -c:v libx264 -r 30 -pix_fmt yuv420 -vf scale=800:600 combinedImages.mp4"
+ffmpeg -framerate $((${1}/${2})) -pattern_type glob -i './downloads/*.jpg' -c:v libx264 -r 30 -pix_fmt yuv420 -vf scale=800:600 combinedImages.mp4"
